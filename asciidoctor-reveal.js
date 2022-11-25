7ea1f6d39e7efb612b7248e1aa6bf6727b224c2f
binout <benoit.prioux@gmail.com>
2022-11-25 23:34:25 +0100

    add bdxio

--- asciidoctor-reveal.js
+++ asciidoctor-reveal.js
@@ -3,8 +3,8 @@ const asciidoctor = require('@asciidoctor/core')()
 const asciidoctorRevealjs = require('@asciidoctor/reveal.js')
 asciidoctorRevealjs.register()
 
-const conferences = ['devoxx', 'breizhcamp']
 // Convert the document 'presentation.adoc' using the reveal.js converter
 const options = { safe: 'safe', backend: 'revealjs' }
 asciidoctor.convertFile('presentation.adoc', {attributes: {conf : 'devoxx'}, ...options})
 asciidoctor.convertFile('presentation.adoc', {attributes: {conf : 'breizhcamp'}, 'to_file': 'presentation-bzh.html',  ...options})
+asciidoctor.convertFile('presentation.adoc', {attributes: {conf : 'bdxio'}, 'to_file': 'presentation-bdxio.html',  ...options})
